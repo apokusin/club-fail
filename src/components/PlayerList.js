558f481193b61f5c9bc7304e056b6af5ae4221ce
Artur Pokusin <git@pokusin.com>
2017-02-18 01:22:53 -0800

    Updates yo

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -3,54 +3,91 @@ import React from 'react';
 import Player from './Player';
 import './PlayerList.css';
 
-const PlayerList = ({players, status, online, error}) => (
-  <ul className="PlayerList">
-    <li
-      key={'controls'}
-      className="PlayerList__controls"
-    >
-      <h2
-        className="PlayerList__title"
-      >
-        {"Who's online"}
-        {error}
-        <div className="PlayerList__subtitle">
-          Join the server by connecting to <code>club.fail</code>
+const PlayerList = (props) => {
+
+  const {
+    status,
+    Playerlist,
+    Players,
+    MaxPlayers,
+    Version,
+  } = props;
+
+  // Add button to display gif when no players
+
+  const ready = status ==="OK";
+
+  return (
+    <div 
+      className={`
+        PlayerList__wrapper 
+        ${ready ? 'PlayerList__wrapper--ready' : '' }
+      `}>
+      {status === "ERR" &&
+        <h2 className="heading PlayerList__title PlayerList__title--error">
+          {"Server is sleeping"}
+        </h2>
+      }
+      {status === "LOADING" &&
+        <div className="heading PlayerList__title PlayerList__title--loading">
+          Loading player list…
         </div>
-      </h2>
-    </li>
-    {online &&
-      players.list.map((player, index) => (
-      <li
-        key={index}
-        className="PlayerList__item"
-      >
-        <Player name={player} />
-      </li>
-    ))}
-    {!online && (
-      <li key={'placeholder'}>
-        <div className="PlayerList__item">
-          No players 😞
+      }
+      {ready &&
+        <h2 className="heading PlayerList__title">
+          {"Online now!"}
+        </h2>
+      }
+      <ul className="PlayerList">        
+        {ready && Players === 0 && (
+          <div className="PlayerList__item PlayerList__item--empty">
+            No players 😞
+          </div>
+        )}
+        {Playerlist &&
+          Playerlist.map((player, index) => (
+            <li
+              key={index}
+              className="PlayerList__item"
+            >
+              <Player name={player} />
+            </li>
+          ))
+        }
+      </ul>
+      {false && ready &&
+        <div className="PlayerList__footer">
+          <div className="PlayerList__join">
+            Join the server by connecting to <code>club.fail</code>
+          </div>
+          <div className="PlayerList__facts">
+            {Version && <div>{'Server version:'} <code>{Version}</code></div>}
+          </div>
         </div>
-      </li>
-    )}
-  </ul>
-);
+      }
+    </div>
+  )
+};
 
 PlayerList.defaultProps = {
-  status: null,
-  players: null,
-  online: null,
-  error: null,
+  Playerlist: false,
+  Players: 0,
+  Version: '',
+  status: 'LOADING',
 }
 
 
 PlayerList.propTypes = {
-  status: React.PropTypes.string,
-  players: React.PropTypes.object,
-  online: React.PropTypes.bool,
-  error: React.PropTypes.string,
+  status: React.PropTypes.oneOf(['OK', 'ERR', 'LOADING']),
+  Playerlist: React.PropTypes.oneOfType([
+    React.PropTypes.bool,
+    React.PropTypes.arrayOf(
+      React.PropTypes.string
+    )
+  ]),
+  Players: React.PropTypes.number,
+  MaxPlayers: React.PropTypes.number,
+  Version: React.PropTypes.string,
 };
 
 export default PlayerList;
