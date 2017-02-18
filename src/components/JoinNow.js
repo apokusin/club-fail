b777512f4993e5b09c1c9ade8a6eb57cef711f1c
Artur Pokusin <git@pokusin.com>
2017-02-18 05:00:25 -0800

    Functioning clipboard copy

--- src/components/JoinNow.js
+++ src/components/JoinNow.js
@@ -1,7 +1,31 @@
 import React, { Component } from 'react';
+import ClipboardButton from 'react-clipboard.js';
+
 import './JoinNow.css';
 
 class JoinNow extends Component {
+  constructor(props) {
+    super(props);
+    this.onSuccess = this.onSuccess.bind(this);
+    this.state = {};
+  }
+  
+  onSuccess() {
+    console.log('copied');
+    this.setState({
+      copied: true
+    })
+
+    if (!this.copyTimeout) {
+      this.copyTimeout = setTimeout(() => {
+        this.setState({
+          copied: false
+        });
+        this.copyTimeout = null;
+      }, 2000);
+    }
+  };
+
   render() {
     return (
       <div>
@@ -18,7 +42,20 @@ class JoinNow extends Component {
             <li>Open Minecraft</li>
             <li>Multiplayer</li>
             <li>Add Server</li>
-            <li>Server address: <code>club.fail</code></li>
+            <li>
+              Server address:
+              <ClipboardButton
+                className={`
+                  Join__ip-button
+                  ${this.state.copied ? 'Join__ip--copied': ''}
+                `}
+                data-clipboard-text="club.fail"
+                button-title="Server address to join club.fail"
+                onSuccess={this.onSuccess}
+              >
+                club.fail
+              </ClipboardButton>
+            </li>
             <li>Done and join!</li>
           </ol>
         </div>
