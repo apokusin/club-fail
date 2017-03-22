b887e9c4cd32bf747d0c170aae8adebda4f2a0d6
Artur Pokusin <git@pokusin.com>
2017-03-21 22:58:17 -0700

    Updated nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -11,8 +11,7 @@ class Nav extends Component {
             <ReactGA.OutboundLink
               className="Nav__button"
               eventLabel="reddit-map"
-              to="https://www.reddit.com/r/ClubFail/comments/5ulesr/what_happened_to_the_map/"
-              target="_blank">
+              to="https://www.reddit.com/r/ClubFail/comments/5ulesr/what_happened_to_the_map/">
               {"Where's the map?"}
             </ReactGA.OutboundLink>
           </li>
@@ -20,8 +19,7 @@ class Nav extends Component {
             <ReactGA.OutboundLink
               className="Nav__button"
               eventLabel="reddit-about"
-              to="https://www.reddit.com/r/ClubFail/comments/5uli8y/about_the_server/"
-              target="_blank">
+              to="https://www.reddit.com/r/ClubFail/comments/5uli8y/about_the_server/">
               {"About the server"}
             </ReactGA.OutboundLink>
           </li>
@@ -29,8 +27,7 @@ class Nav extends Component {
             <ReactGA.OutboundLink
               className="Nav__button"
               eventLabel="reddit-backpacks"
-              to="https://www.reddit.com/r/ClubFail/comments/5ulikn/how_to_use_backpacks/"
-              target="_blank">
+              to="https://www.reddit.com/r/ClubFail/comments/5ulikn/how_to_use_backpacks/">
               {"Learn about backpacks"}
             </ReactGA.OutboundLink>
           </li>
