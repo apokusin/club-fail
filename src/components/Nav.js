d10816bc3a9f028316fa7df0dda87c491c352c0b
Artur Pokusin <apokusin@users.noreply.github.com>
2018-03-01 19:25:05 -0800

    Firebase (#21)
    * Added firebase
    
    * Updates
    
    * More
    
    * deploy
    
    * Removed Minecraft page and added shows

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import ReactGA from 'react-ga';
+
+import LogoFail from '../images/art-talkingfail.png';
+import LogoDisco from '../images/art-discographers.jpg';
+import LogoCraigslist from '../images/art-craigslist.jpg';
 import './Nav.css';
 
 class Nav extends Component {
@@ -10,9 +14,43 @@ class Nav extends Component {
           <li>
             <ReactGA.OutboundLink
               className="Nav__button"
-              eventLabel="reddit-about"
-              to="https://www.reddit.com/r/ClubFail/comments/5uli8y/about_the_server/">
-              {"About the server"}
+              eventLabel="show-talking"
+              to="http://www.talking.fail">
+              <img
+                src={LogoFail}
+                height="64"
+                width="64"
+                alt="Talking Fail Podcast Logo"
+              />
+              Talking Fail Podcast
+            </ReactGA.OutboundLink>
+          </li>
+          <li>
+            <ReactGA.OutboundLink
+              className="Nav__button"
+              eventLabel="show-discographers"
+              to="https://thediscographers.simplecast.fm">
+              <img
+                src={LogoDisco}
+                height="64"
+                width="64"
+                alt="The Discographers Podcast Logo"
+              />
+              The Discographers Podcast
+            </ReactGA.OutboundLink>
+          </li>
+          <li>
+            <ReactGA.OutboundLink
+              className="Nav__button"
+              eventLabel="show-craigslist"
+              to="https://www.youtube.com/watch?v=h4oGzc8Z5yU&feature=youtu.be">
+              <img
+                src={LogoCraigslist}
+                height="64"
+                width="64"
+                alt="Cruisin' Craigslist Logo"
+              />
+              Cruisin' Craigslist
             </ReactGA.OutboundLink>
           </li>
         </ul>
